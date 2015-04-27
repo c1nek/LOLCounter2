aabfc82bf3d1cf2282994f74b790c754ce34aaf1
c1nek <gumball300@gmail.com>
2015-04-27 14:31:17 +0200

    xaxa

---
+++ app/src/main/java/com/dzordan/lolcounter/champ_activity.java
@@ -0,0 +1,20 @@
+package com.dzordan.lolcounter;
+
+
+import android.os.Bundle;
+
+
+/**
+ * Created by Marcin on 2015-04-27.
+ */
+public class champ_activity extends login_activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.champ_layout);
+
+    }
+
+
+}
--- app/src/main/java/com/dzordan/lolcounter/stats_activity.java
+++ app/src/main/java/com/dzordan/lolcounter/stats_activity.java
@@ -1,18 +1,42 @@
 package com.dzordan.lolcounter;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 /**
  * Created by Marcin on 2015-04-27.
  */
 public class stats_activity extends login_activity {
 
+    TableLayout table;
+
+
+    LinearLayout linearLay;
+    TableRow row;
+
+    View v;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stats_activity_layout);
+
+        table = (TableLayout) findViewById(R.id.mainTable);
+        //linearLay = (LinearLayout) findViewById(R.id.linearLay);
+
+        row = (TableRow) findViewById(R.id.champLay);
+
+        //row.
+
+        int BASEID=200;
+
+        //View v = mLayoutInflator.inflate(R.layout.myRelativeLayout, null);
+
+
+       // table.
 
         // Get the message from the intent
        // Intent intent = getIntent();
